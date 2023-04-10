50b26b40bafbd73240ac3f487ad9a9dacb620001
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2023-04-10 08:37:46 -0300

    add: Telas de home, leituras e configutações

--- MinhaLeitura/SRC/Controller/FrontController.java
+++ MinhaLeitura/SRC/Controller/FrontController.java
@@ -5,21 +5,25 @@ import SRC.Model.VO.User;
 import SRC.View.Telas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class FrontController {
     @FXML private Label avisoLogin;
     @FXML private TextField username;
     @FXML private PasswordField password;
 
+
     private User usuario;
     private UserDAO usuarioDAO = new UserDAO();
 
     public void autenticar(ActionEvent event) throws Exception{
-        avisoLogin.setVisible(true);
-        //Telas.telaInicial();
+        //avisoLogin.setVisible(true);
+        Telas.telaInicial();
     }
 
     public void fazerCadastro(ActionEvent event) throws Exception{
@@ -28,5 +32,32 @@ public class FrontController {
 
     public void sair(ActionEvent event) throws Exception{
         Telas.telaLogin();
+    }
+
+    public void telaInicial(ActionEvent event) throws  Exception{
+        Telas.telaInicial();
+    }
+
+    public void telaLeituras(ActionEvent event) throws  Exception{
+        Telas.telaLeituras();
+    }
+
+    public void telaLeiturasFinalizadas(ActionEvent event) throws  Exception{
+        Telas.telaLeiturasFinalizadas();
+    }
+
+    public void telaConfiguracoes(ActionEvent event) throws  Exception{
+        Telas.telaConfiguracoes();
+    }
+
+    public void abrirModalAddLeitura(ActionEvent event) throws  Exception{
+        Telas.modalAddLeitura();
+    }
+
+    @FXML
+    private Button fecharTela;
+    public void fecharModalAddLeitura(ActionEvent event) throws Exception{
+        Stage stage = (Stage) fecharTela.getScene().getWindow(); //Obtendo a janela atual
+        stage.close(); //Fechando o Stage
     }
 }
--- MinhaLeitura/SRC/View/Telas.java
+++ MinhaLeitura/SRC/View/Telas.java
@@ -5,7 +5,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 public class Telas extends Application {
     private static Stage primaryStage;
@@ -36,6 +42,38 @@ public class Telas extends Application {
         Parent root = FXMLLoader.load(Telas.class.getResource("VE/Home.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+    }
+
+    public static void telaLeituras() throws Exception{
+        Parent root = FXMLLoader.load(Telas.class.getResource("VE/EmLeitura.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+    }
+
+    public static void telaLeiturasFinalizadas() throws Exception {
+        Parent root = FXMLLoader.load(Telas.class.getResource("VE/Finalizadas.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+    }
+
+    public static void telaConfiguracoes() throws Exception{
+        Parent root = FXMLLoader.load(Telas.class.getResource("VE/Configuracoes.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+    }
+
+    public static void modalAddLeitura() throws Exception{
+        Stage newStage = new Stage();
+        //newStage.initStyle(StageStyle.TRANSPARENT);
+        newStage.setTitle("Nova Leitura");
+
+        newStage.initOwner(Telas.primaryStage);
+        newStage.initModality(Modality.APPLICATION_MODAL);
+
+        Parent root = FXMLLoader.load(Telas.class.getResource("VE/modalAddLeitura.fxml"));
+        Scene cena = new Scene(root);
+        newStage.setScene(cena);
+        newStage.show();
     }
 
     public static void main(String ... args) {
